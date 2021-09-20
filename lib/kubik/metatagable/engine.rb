516ae40b3565cc436775d34a9647c3e7024f34b6
cfbonner <cfbonner@gmail.com>
2021-09-20 13:50:30 +0100

    Update engine loader

--- lib/kubik/metatagable/engine.rb
+++ lib/kubik/metatagable/engine.rb
@@ -3,9 +3,11 @@ require 'rails-settings-cached'
 module Kubik
   module Metatagable
     class Engine < ::Rails::Engine
-      initializer :extend_application_controller do
-        ActionController::Base.send :include, Kubik::MetatagableController
-        ActionController::Base.send :before_action, :fetch_meta_tags
+      config.after_initialize do
+        ActiveSupport.on_load(:action_controller_base) do
+          include Kubik::MetatagableController
+          before_action :fetch_meta_tags
+        end
       end
     end
   end
